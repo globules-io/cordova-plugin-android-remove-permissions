d675f30bdb8066bdda398a13abf06a8784580e8b
Eric <info@ericparlier.com>
2022-09-25 14:48:40 -0400

    Inline promise

--- scripts/remove_permissions.js
+++ scripts/remove_permissions.js
@@ -3,12 +3,12 @@ const xml2js = require('xml2js');
 const root = context.opts.projectRoot;
 const configPath = 'config.xml';
 const configXml = fs.readFileSync(configPath);
-let config;
+let config, manifest;
 let unwantedPermissions = [];
 let permissionRemoved = [];
 //Config
-xml2js.parseStringPromise(configXml).then(function(result){
-	config = result;
+xml2js.parseString(configXml).then(function(__err, __res){
+	config = __res;
 	for(let i = 0; i < config.widget.preference.length; i++){	 
 		if(config.widget.preference[i]['$'].name === 'AndroidRemovePermissions'){
 			unwantedPermissions = config.widget.preference[i]['$'].value.split(',');
@@ -17,21 +17,23 @@ xml2js.parseStringPromise(configXml).then(function(result){
 	}
 	if(unwantedPermissions.length){            
         const manifestPath = root + '/platforms/android/app/src/main/AndroidManifest.xml';
-        const manifestXml = fs.readFileSync(manifestPath);
-        const manifest = xml2js.parseString(manifestXml);
-        const usesPermissions = manifest.manifest['uses-permission'];
-        if(Array.isArray(usesPermissions)){
-            manifest.manifest['uses-permission'] = usesPermissions.filter(usesPermission => {
-                const attrs = usesPermission.$ || {};
-                const name = attrs['android:name'] ;
-                if(unwantedPermissions.includes('android.permission.'+name)){
-                    permissionRemoved.push(name);				
-                    return false;
-                }else{
-                    return true;
-                }
-            });
-        }
+        const manifestXml = fs.readFileSync(manifestPath);       
+        xml2js.parseString(manifestXml, function(__err, __res){
+            manifest = __res;
+            const usesPermissions = manifest.manifest['uses-permission'];
+            if(Array.isArray(usesPermissions)){
+                manifest.manifest['uses-permission'] = usesPermissions.filter(usesPermission => {
+                    const attrs = usesPermission.$ || {};
+                    const name = attrs['android:name'] ;
+                    if(unwantedPermissions.includes('android.permission.'+name)){
+                        permissionRemoved.push(name);				
+                        return false;
+                    }else{
+                        return true;
+                    }
+                });
+            }
+        });        
         //log
         cosole.log('cordova-plugin-android-remove-permissions found and removed ', permissionRemoved.length, 'permissions: ', permissionRemoved.join(', '));
         //Save
