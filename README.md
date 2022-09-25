a754a620576778ca1f4a407be59234da982508dd
Eric <info@ericparlier.com>
2022-09-25 14:33:09 -0400

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cordova-plugin-android-remove-permissions
 Plugin for Cordova Android to remove unwanted permissions
+
+## Usage
+> Add the unwanted permissions to the preference
+
+     <preference name="AndroidRemovePermissions" value="INTERNET,BLUETOOTH" />
---
+++ scripts/remove_permissions.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+const root = context.opts.projectRoot;
+const configPath = 'config.xml';
+const configXml = fs.readFileSync(configPath);
+let config;
+let unwantedPermissions = [];
+let permissionRemoved = [];
+//Config
+xml2js.parseStringPromise(configXml).then(function(result){
+	config = result;
+	for(let i = 0; i < config.widget.preference.length; i++){	 
+		if(config.widget.preference[i]['$'].name === 'AndroidRemovePermissions'){
+			unwantedPermissions = config.widget.preference[i]['$'].value.split(',');
+			break;
+		}		 
+	}
+	if(unwantedPermissions.length){            
+        const manifestPath = root + '/platforms/android/app/src/main/AndroidManifest.xml';
+            const manifestXml = await fs.readFile(manifestPath);
+            const manifest = await xml2js.parseStringPromise(manifestXml);
+            const usesPermissions = manifest.manifest['uses-permission'];
+            if(Array.isArray(usesPermissions)) {
+            manifest.manifest['uses-permission'] = usesPermissions.filter(usesPermission => {
+                const attrs = usesPermission.$ || {};
+                const name = attrs['android:name'] ;
+                if (unwantedPermissions.includes('android.permission.'+name)){
+                    permissionRemoved.push(name);				
+                    return false;
+                }else{
+                    return true;
+                }
+            });
+        }
+        //log
+        cosole.log('cordova-plugin-android-remove-permissions found and removed ', permissionRemoved.length, 'permissions: ', permissionRemoved.join(', '));
+        //Save
+        const newManifest = (new xml2js.Builder()).buildObject(manifest);
+        fs.writeFileSync(manifestPath, newManifest);	
+    }
+		
+});
